77ef382d919a5dc20dfd77f0c4f81c8e94f7d06a
Caleb Yoon <cyoon11@umd.edu>
2023-04-27 16:59:31 -0400

    added new features

--- main.js
+++ main.js
@@ -42,19 +42,44 @@ d3.csv("dataset.csv").then(function(data) {
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(yScale));
+  
+  // Add Y axis label
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("font-size", "10px")
+    .text("GDP per capita in $ (PPP) 2021");
 
 
+
+  let baseline_value; 
   // Bars
   svg.selectAll(".bar")
     .data(data)
     .enter()
     .append("rect")
       .attr("class", "bar")
-      .attr("x", function(d) { return xScale(d.Country); })
+      .attr("x", function(d) {
+        if(d.Country === "United States") {
+          baseline_value = yScale(+d["GDP per capita in $ (PPP) 2021"]);
+        }
+        return xScale(d.Country); 
+      })
       .attr("y", function(d) { return yScale(+d["GDP per capita in $ (PPP) 2021"]); })
       .attr("width", xScale.bandwidth())
       .attr("height", function(d) { return height - yScale(+d["GDP per capita in $ (PPP) 2021"]); })
       .attr("fill", "#69b3a2")
+
+  svg.append("line")
+    .attr("x1", 0)
+    .attr("y1", baseline_value)
+    .attr("x2", width)
+    .attr("y2", baseline_value)
+    .style("stroke", "#999")
+    .style("stroke-dasharray", ("3, 3"));
 
 
 })
